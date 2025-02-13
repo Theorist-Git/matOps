3ca6268dac8b45dee4424405ad3710b3f95790e2
Mayank Vats <theorist@pop-os.localdomain>
2025-02-13 23:44:38 +0530

    Added container indexing, == and - operations

--- src/linAlg.cpp
+++ src/linAlg.cpp
@@ -19,16 +19,16 @@ typedef class Matrix {
             
             // Base case: 2x2 matrix
             if (n == 2) {
-                return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+                return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
             }
             
             double det = 0.0;
 
-            for (int col = 0; col < n; col++) {
+            for (size_t col = 0; col < n; col++) {
                 std::vector<std::vector<double>> minor;
-                for (int i = 1; i < n; i++) {
+                for (size_t i = 1; i < n; i++) {
                     std::vector<double> row;
-                    for (int j = 0; j < n; j++) {
+                    for (size_t j = 0; j < n; j++) {
                         if (j == col)
                             continue;
                         row.push_back(matrix[i][j]);
@@ -82,6 +82,40 @@ typedef class Matrix {
             return addRes;
         }
 
+        Matrix operator-(const Matrix& other) const {
+            if (this->nrows != other.nrows || this->ncols != other.ncols) {
+                throw std::invalid_argument("Matrix dims don't align");
+            }
+
+            Matrix addRes = *this; // addRes = A in A + B
+
+            for (size_t i = 0; i < this->nrows; ++i) {
+                for (size_t j = 0; j < this->ncols; ++j) {
+                    addRes.container[i][j] -= other.container[i][j];
+                }
+            }
+
+            return addRes;
+        }
+
+        bool operator==(const Matrix& other) const {
+            if (this->nrows != other.nrows || this->ncols != other.ncols) {
+                return false;
+            }
+
+            constexpr double EPS = 1e-9;
+
+            for (size_t i = 0; i < this->nrows; ++i) {
+                for (size_t j = 0; j < this->ncols; ++j) {
+                    if ( std::abs(this->container[i][j] - other.container[i][j]) > EPS) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
         Matrix operator*(const Matrix& other) const {
             if (this->ncols != other.nrows) {
                 std::cout << this->ncols << " " << other.nrows << std::endl;
@@ -103,6 +137,14 @@ typedef class Matrix {
             }
 
             return Matrix(mulResContainer);
+        }
+
+        double operator()(size_t row, size_t col) {
+            if (row >= this->nrows || col >= this->ncols) {
+                throw std::out_of_range("Index out of bounds");
+            }
+
+            return this->container[row][col];
         }
 
         void transpose() {
@@ -140,6 +182,54 @@ typedef class Matrix {
             return _det(this->container);
         }
 
+        Matrix inverse() {
+            double det = this->determinant();
+
+            if (det == 0) {
+                throw std::runtime_error("Singular matrix");
+            }
+
+            std::vector<std::vector<double>> cof(this->nrows, std::vector<double>(this->ncols, 0.0));
+
+            for (size_t i = 0; i < this->nrows; ++i) {
+                for (size_t j = 0; j < this->ncols; ++j) {
+
+                    std::vector<std::vector<double>> minor;
+
+                    for (size_t mi = 0; mi < this->nrows; ++mi) {
+
+                        if (mi == i) continue;
+
+                        std::vector<double> row;
+
+                        for (size_t mj = 0; mj < this->ncols; ++mj) {
+                            if (mj == j) continue;
+
+                            row.push_back(this->container[mi][mj]);
+                        }
+
+                        minor.push_back(row);
+                    }
+
+                    double minorDet = _det(minor);
+                    double sign = ((i + j) % 2 == 0) ? 1.0 : -1.0;
+
+                    cof[i][j] = sign * minorDet;
+                }
+            }
+
+            Matrix inv(cof);
+            inv.transpose();
+
+            for (size_t i = 0; i < this->nrows; ++i) {
+                for (size_t j = 0; j < this->ncols; ++j) {
+                    inv.container[i][j] = inv.container[i][j] / det;
+                }
+            }
+
+            return inv;
+        }
+
         void showMatrix() {
             std::cout << "[\n";  
 
@@ -162,7 +252,4 @@ typedef class Matrix {
         
             std::cout << "]\n";  
         }
-
-        
-
 } mat;
